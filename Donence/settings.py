2cfd111456ac488f55fb74cad4059b4dd9e23d78
mrtyildiz <muratasnb@hotmail.com>
2021-12-10 23:03:36 +0300

    Update settings.py

--- Donence/settings.py
+++ Donence/settings.py
@@ -102,15 +102,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT  = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-MEDIA_ROOT  = os.path.join(PROJECT_ROOT, 'media')
+STATIC_URL = 'app/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
